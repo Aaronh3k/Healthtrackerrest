835dac40eb143b8aeb56e5f89d2342748565c358
aaron <20100312@mail.wit.ie>
2022-10-25 16:09:27 +0100

    lab 05

---
+++ src/main/kotlin/ie/setu/config/DbConfig.kt
@@ -0,0 +1,28 @@
+package ie.setu.config
+
+import mu.KotlinLogging
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.name
+
+class DbConfig{
+
+    private val logger = KotlinLogging.logger {}
+
+    //NOTE: you need the ?sslmode=require otherwise you get an error complaining about the ssl certificate
+    fun getDbConnection() :Database{
+
+        logger.info{"Starting DB Connection..."}
+
+        val dbConfig = Database.connect(
+            "jdbc:postgresql://ec2-54-173-237-110.compute-1.amazonaws.com:5432/db312aubut6p30?sslmode=require",
+            driver = "org.postgresql.Driver",
+            user = "kfollkuorwkkma",
+            password = "070dff8fb882b22fc9ab0146e9d57435b7c1e3627f08f2a47adaf018e52b3f49")
+
+        logger.info{"DbConfig name = " + dbConfig.name}
+        logger.info{"DbConfig url = " + dbConfig.url}
+
+        return dbConfig
+    }
+
+}
--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -3,18 +3,32 @@ package ie.setu.config
 import ie.setu.controllers.HealthTrackerController
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.*
-
+import io.javalin.plugin.openapi.ui.SwaggerOptions
+import io.javalin.plugin.openapi.OpenApiOptions
+import io.javalin.plugin.openapi.OpenApiPlugin
+import io.javalin.plugin.openapi.ui.ReDocOptions
+import io.swagger.v3.oas.models.info.Info
 class JavalinConfig {
 
     fun startJavalinService(): Javalin {
 
-        val app = Javalin.create().apply {
-            exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
+        val app = Javalin.create {
+            it.registerPlugin(getConfiguredOpenApiPlugin())
+            it.defaultContentType = "application/json"
+        }.apply {
+            exception(Exception::class.java) { e, _ -> e.printStackTrace() }
             error(404) { ctx -> ctx.json("404 - Not Found") }
-        }.start(7000)
+        }.start(getHerokuAssignedPort())
 
         registerRoutes(app)
         return app
+    }
+
+    private fun getHerokuAssignedPort(): Int {
+        val herokuPort = System.getenv("PORT")
+        return if (herokuPort != null) {
+            Integer.parseInt(herokuPort)
+        } else 7000
     }
 
     private fun registerRoutes(app: Javalin) {
@@ -33,4 +47,18 @@ class JavalinConfig {
             }
         }
     }
+
+    fun getConfiguredOpenApiPlugin() = OpenApiPlugin(
+        OpenApiOptions(
+            Info().apply {
+                title("Health Tracker App")
+                version("1.0")
+                description("Health Tracker API")
+            }
+        ).apply {
+            path("/swagger-docs") // endpoint for OpenAPI json
+            swagger(SwaggerOptions("/swagger-ui")) // endpoint for swagger-ui
+            reDoc(ReDocOptions("/redoc")) // endpoint for redoc
+        }
+    )
 }
